af6e7eb0e5a366c6306ce19ed1b9213e72dd5634
wjunhee102 <wjunhee102@gmail.com>
2020-04-03 17:05:39 +0900

    Updates

---
+++ precache-manifest.5e500173d06bea796ca28454e17471d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f46ad1dbd20f8424c88b7a4c788da0f",
+    "url": "/movieapp/index.html"
+  },
+  {
+    "revision": "cf6ea524849331ad51ba",
+    "url": "/movieapp/static/js/2.266d80f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movieapp/static/js/2.266d80f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d407cd9b2cd8cc40444d",
+    "url": "/movieapp/static/js/main.a56ff1fb.chunk.js"
+  },
+  {
+    "revision": "863e9525a9b94e682e48",
+    "url": "/movieapp/static/js/runtime-main.97decdef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movieapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
